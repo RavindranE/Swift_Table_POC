d9e9cfea78359910b2bbafc7bb67e0dc92372cc5
resakk611 <ravindran_esakkimuthu@comcast.com>
2020-02-12 10:46:41 +0530

    Initial Commit

---
+++ iOSSwiftTableDisplay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSSwiftTableDisplay
+//
+//  Created by Ravindran Esakkimuthu on 12/02/20.
+//  Copyright © 2020 Ravindran Esakkimuthu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
